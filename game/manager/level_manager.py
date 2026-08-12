6a9bebe5d0515ef239fe7a9e86b984ce4247227e
Andrea <foriglio.andrea@gmail.com>
2026-08-12 18:16:12 +0200

    funzioni di level_manager

--- game/manager/level_manager.py
+++ game/manager/level_manager.py
@@ -1,2 +1,20 @@
 class LevelManager:
-    pass
+    def __init__(self):
+        pass
+    
+    def add_exp(self, player, amount):
+        player.exp += amount
+        
+    
+    def can_level_up(self, player):
+        return player.exp >= player.exp_to_next_level
+        
+    
+    
+    def level_up(self, player):
+        player.level += 1
+        player.exp = 0
+        
+        # Funzione player per gestire gli aumenti delle statistiche
+        
+        print(f"{player.name} reached level {player.level}!")
